902b39292d537e2044876d67f0231c5e435666aa
bi0punk <systecinfo_cl@criptext.com>
2024-10-31 23:58:58 -0300

    add json format output

--- app.py
+++ app.py
@@ -5,14 +5,8 @@ def docker_ps_json():
     try:
         # Ejecuta el comando 'docker ps -a'
         result = subprocess.run(['docker', 'ps', '-a', '--format', '{{json .}}'], capture_output=True, text=True, check=True)
-        
-        # Separa las líneas de la salida
         lines = result.stdout.strip().split('\n')
-        
-        # Convierte cada línea de JSON a un diccionario
         containers = [json.loads(line) for line in lines]
-        
-        # Convierte la lista de contenedores a JSON
         json_output = json.dumps(containers, indent=4)
         return json_output
     
